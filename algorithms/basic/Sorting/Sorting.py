2bacff00338ff2a19829c200be512f137ef660fb
huangyu1996 <sdu_huangyu@foxmail.com>
2018-09-20 13:08:07 +0800

    interview

--- algorithms/basic/Sorting/Sorting.py
+++ algorithms/basic/Sorting/Sorting.py
@@ -11,42 +11,80 @@ class SortingSolution:
          swap(j,j+1)          //两两排序（升序/降序）
          '''
 
-        for i in range(len(A)-1,-1,-1):
+        for i in range(len(A) - 1, -1, -1):
             for j in range(i):
-                if(A[j]>A[j+1]):A[j],A[j+1]=A[j+1],A[j]
+                if (A[j] > A[j + 1]): A[j], A[j + 1] = A[j + 1], A[j]
         return A
 
-
-    def SelectionSort(self,A):
+    def SelectionSort(self, A):
 
         for i in range(len(A)):
             minIndex = i
-            for j in range(i+1,len(A)):
-                if A[j]<A[minIndex]:
-                    A[minIndex],A[j] = A[j],A[minIndex]
+            for j in range(i + 1, len(A)):
+                if A[j] < A[minIndex]:
+                    A[minIndex], A[j] = A[j], A[minIndex]
         return A
 
-    def InsertionSort(self,A):
+    def InsertionSort(self, A):
 
-        for i in range(1,len(A)):
-            for j in range(i-1,-1,-1):
-                if(j>=0 and A[j]>A[j+1]):A[j],A[j+1]=A[j+1],A[j]
+        for i in range(1, len(A)):
+            for j in range(i - 1, -1, -1):
+                if (j >= 0 and A[j] > A[j + 1]): A[j], A[j + 1] = A[j + 1], A[j]
 
         return A
 
-    def QuickSort(self,A):
+    def QuickSort(self, A, begin, end):
+        if len(A) > 1:
+            split = self.partition2(A)
+            self.QuickSort(A, begin, split)
+            self.QuickSort(A, split + 1, end)
+
+    def partition(self, arr, pivot):
         pass
 
+    def partition2(self, arr):
+        if len(arr) > 1:
+            pivot = arr[0]
+            less, more, cur = 0, len(arr) - 1, 0
+            while (cur <= more):
+                if arr[cur] < pivot:
+                    arr[less], arr[cur] = arr[cur], arr[less]
+                    cur += 1
+                    less += 1
+                    continue
+                elif arr[cur] == pivot:
 
-    def ShellSort(self,A):
+                    cur += 1
+
+                else:
+
+                    arr[more], arr[cur] = arr[cur], arr[more]
+                    more -= 1
+
+        return [less, more]  # 返回==pivot的左右边界
+        # if arr==[]:
+        #     return
+        # i=0
+        # j=len(arr)-1
+        # k=0
+        # while k<=j:#一开始用k<len(nums)出现了错误list index out of range
+        #     if arr[k]==0:
+        #         arr[k],arr[i]=arr[i],arr[k]
+        #         k+=1
+        #         i+=1
+        #     elif arr[k]==2:
+        #         arr[k],arr[j]=arr[j],arr[k]
+        #         j-=1
+        #     else:
+        #         k+=1
+
+    def ShellSort(self, A):
         pass
 
-
-    def HeapSort(self,A):
+    def HeapSort(self, A):
         pass
 
-
-    def MergeSort(self,A):
+    def MergeSort(self, A):
         if len(A) > 1:
 
             mid = int(len(A) / 2)
@@ -78,12 +116,14 @@ class SortingSolution:
                 k += 1
                 j += 1
 
-    def right_method(self,A):
+    def rightest_method(self, A):
         A.sort()
 
 
+
+
 if __name__ == '__main__':
-    seq = [4, 5, 7, 9, 7, 5, 1, 0, 7, -2, 3, -99, 6]
+    seq = [2, 0, 1, 49, 20, -8]
     s = SortingSolution()
-    s.MergeSort(seq)
+    print(s.partition2(seq))
     print(seq)
