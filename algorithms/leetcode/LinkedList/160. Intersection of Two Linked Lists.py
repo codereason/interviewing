1b958f3c5a0596dc10e5c42ca1834728ce29a46d
huangyu1996 <sdu_huangyu@foxmail.com>
2018-09-29 17:09:30 +0800

    interview

--- algorithms/leetcode/LinkedList/160. Intersection of Two Linked Lists.py
+++ algorithms/leetcode/LinkedList/160. Intersection of Two Linked Lists.py
@@ -18,14 +18,18 @@ If the two linked lists have no intersection at all, return null.
 The linked lists must retain their original structure after the function returns.
 You may assume there are no cycles anywhere in the entire linked structure.
 Your code should preferably run in O(n) time and use only O(1) memory.
+
+
+方法：先确定两个链表的长度，将较长的链表截掉较长的那一部分lenA - lenB，这样子两个链表长度相等，即可while比较
 '''
 
 
 # Definition for singly-linked list.
-class ListNode(object):
+class Node(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
@@ -33,4 +37,39 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
+        if (headA is None or headB is None):
+            return None
+        lenA = self.getLength(headA)
+        lenB = self.getLength(headB)
+        if (lenA > lenB):
+            while (lenA != lenB):
+                headA = headA.next
+                lenA -= 1
+        if (lenA < lenB):
+            while (lenA != lenB):
+                headB = headB.next
+                lenB -= 1
+        while (headA != headB):
+            headA = headA.next
+            headB = headB.next
+        return headA
 
+    def getLength(self, head):
+        count = 0
+        while (head != None):
+            count += 1
+            head = head.next
+        return count
+
+# if __name__ == "__main__":
+#     node1 = Node(1)
+#     node2 = Node(2)
+#     node3 = Node(3)
+#     node4 = Node(4)
+#     node5 = Node(5)
+#     node1.next = node2
+#     node2.next = node3
+#     node3.next = node4
+#     node4.next = node5
+#     node5.next = node2
+#     print(Solution().hasCycle(node1))
