38fb6f56025551b06cd808ff1ac1957319c4358e
huangyu1996 <sdu_huangyu@foxmail.com>
2018-10-12 09:49:12 +0800

    interview

--- algorithms/basic/岛问题.py
+++ algorithms/basic/岛问题.py
@@ -31,27 +31,38 @@
 
 '''
 
+List = [["1", "1", "1", "1", "0"], ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"], ["0", "0", "0", "0", "0"]]  # 有三个岛
 
-List = [[0,0,1,0,1,0],
-        [1,1,1,0,1,0],
-        [1,0,0,1,0,0],
-        [0,0,0,0,0,0]]  #有三个岛
-
-#遍历所有的点，遇到1感染这些1区域，把1全部改为2，岛的数量+1；
-#遍历剩下的非2区域，继续这样做
-print (len(List[0]))
-class Resoultion:
-    def countIslands(self,areas):
-        N = len(areas)
-        M = len(areas[0])
-        count = 0
-        for i in range(N):
-            for j in range(M):
-                if areas[i][j] == 1:
-                    count+=1
-                    self.infect(i,j,N,M)
-        return count
+# 遍历所有的点，遇到1感染这些1区域，把1全部改为2，岛的数量+1；
+# 遍历剩下的非2区域，继续这样做
+print(len(List[0]))
 
 
-    def infect(self,i,j,N,M):
-        if (i > 1 or j > 1 or i)
+class Solution:
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        res = 0
+        M = len(grid)
+        if M > 0:
+            N = len(grid[0])
+
+            # print
+            for i in range(M):
+                for j in range(N):
+                    if grid[i][j] is "1":
+                        res += 1
+                        self.infect(i, j, M, N, grid)
+
+        return res
+
+    def infect(self, i, j, M, N, grid):
+        if i >= 0 and j >= 0 and i < M and j < N and grid[i][j] == '1':
+            grid[i][j] = '2'
+            self.infect(i - 1, j, M, N, grid)
+            self.infect(i + 1, j, M, N, grid)
+            self.infect(i, j - 1, M, N, grid)
+            self.infect(i, j + 1, M, N, grid)
