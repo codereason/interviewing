3a38cc77398205e15b7479bf83db907c8dab9b5c
huangyu1996 <sdu_huangyu@foxmail.com>
2018-10-18 21:25:36 +0800

    interview

--- algorithms/leetcode/Array/15. 3Sum.py
+++ algorithms/leetcode/Array/15. 3Sum.py
@@ -31,7 +31,9 @@ class Solution(object):
         nums.sort()
         # print(nums)
         for i in range(len(nums) - 2):
-
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+                ###加一个判断 判断是否这个target数曾经出现过 如果出现过直接跳过去 可以省去时间避免TLE
             left, right = i + 1, len(nums) - 1
             while (left < right):
 
@@ -57,28 +59,24 @@ class Solution2(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        N = len(nums)
         nums.sort()
-        res = []
-        for t in range(N - 2):
-            if t > 0 and nums[t] == nums[t - 1]:
-                    continue
-            i, j = t + 1, N - 1
-            while i < j:
-                _sum = nums[t] + nums[i] + nums[j]
-                if _sum == 0:
-                    res.append([nums[t], nums[i], nums[j]])
-                    i += 1
-                    j -= 1
-                    while i < j and nums[i] == nums[i - 1]:
-                        i += 1
-                    while i < j and nums[j] == nums[j + 1]:
-                        j -= 1
-                elif _sum < 0:
-                    i += 1
+        N, result = len(nums), []
+        for i in range(N):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            target = nums[i]*-1
+            s,e = i+1, N-1
+            while s<e:
+                if nums[s]+nums[e] == target:
+                    result.append([nums[i], nums[s], nums[e]])
+                    s = s+1
+                    while s<e and nums[s] == nums[s-1]:
+                        s = s+1
+                elif nums[s] + nums[e] < target:
+                    s = s+1
                 else:
-                    j -= 1
-        return res
+                    e = e-1
+        return result
 '''--------------------- 
 作者：负雪明烛 
 来源：CSDN 
