e8e53f00c82df8e0a7985da7368b5c4df4d0873d
huangyu1996 <sdu_huangyu@foxmail.com>
2018-10-18 21:08:25 +0800

    interview

--- algorithms/leetcode/Array/15. 3Sum.py
+++ algorithms/leetcode/Array/15. 3Sum.py
@@ -17,7 +17,9 @@ A solution set is:
 ]
 
 '''
-
+'''
+会超时
+'''
 
 class Solution(object):
     def threeSum(self, nums):
@@ -49,8 +51,40 @@ class Solution(object):
                     continue
 
         return res
-
-
+class Solution2(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        N = len(nums)
+        nums.sort()
+        res = []
+        for t in range(N - 2):
+            if t > 0 and nums[t] == nums[t - 1]:
+                    continue
+            i, j = t + 1, N - 1
+            while i < j:
+                _sum = nums[t] + nums[i] + nums[j]
+                if _sum == 0:
+                    res.append([nums[t], nums[i], nums[j]])
+                    i += 1
+                    j -= 1
+                    while i < j and nums[i] == nums[i - 1]:
+                        i += 1
+                    while i < j and nums[j] == nums[j + 1]:
+                        j -= 1
+                elif _sum < 0:
+                    i += 1
+                else:
+                    j -= 1
+        return res
+'''--------------------- 
+作者：负雪明烛 
+来源：CSDN 
+原文：https://blog.csdn.net/fuxuemingzhu/article/details/83115850 
+版权声明：本文为博主原创文章，转载请附上博文链接！
+'''
 if __name__ == '__main__':
     nums = []
     print(Solution().threeSum(nums))
