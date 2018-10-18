d800a5b8cdda1eff7e1e1a206f0c96c19244524b
hy <sdu_huangyu@foxmail.com>
2018-10-18 22:54:24 +0800

    interview

---
+++ algorithms/swordtooffer/Ch.02/问题5：从尾到头打印链表.py
@@ -0,0 +1,24 @@
+def printLinkedListReverse(root):
+    List = []
+    if root is not None:
+        while(root ):
+            List.append(root.val)
+            root = root.next
+        return List[::-1]
+
+class Node():
+    def __init__(self, val):
+        self.next = None
+        self.val = val
+if __name__ == '__main__':
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next=None
+    print(printLinkedListReverse(node1))
