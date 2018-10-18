8126489e79472158ac58966706554924cd158db8
hy <sdu_huangyu@foxmail.com>
2018-10-18 22:22:40 +0800

    interview

---
+++ algorithms/swordtooffer/Ch.02/问题4：替换空格.py
@@ -0,0 +1,15 @@
+import re
+def replaceSpaces(s):
+    return s.replace(" ","%20")
+
+def replaceSpaces2(s):
+    lst = list(s)
+    for item in lst:
+        if item == ' ':
+            lst[lst.index(item)]="%20"
+    print(lst)
+    return ''.join(lst)
+def classic
+if __name__ == '__main__':
+    s = "we are young"
+    print(replaceSpaces2(s))
