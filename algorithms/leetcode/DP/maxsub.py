28dc131c6b1580b1fc8ef3d033213d856e726e01
yuhuang1996 <sdu_huangyu@foxmail.com>
2019-04-24 16:24:24 +0800

    dp

--- algorithms/leetcode/DP/maxsub.py
+++ algorithms/leetcode/DP/maxsub.py
@@ -22,7 +22,7 @@ dp思想
     dp[0] = array[0]
     for i in range(1,len(array)):
         dp[i]=dp[i-1]+array[i] if dp[i-1]>0 else array[i]
-    return dp
+    return max(dp)
 
 if __name__ == '__main__':
     print(FindGreatestSumOfSubArray([6,-3,-2,7,-15,1,2,2]))
