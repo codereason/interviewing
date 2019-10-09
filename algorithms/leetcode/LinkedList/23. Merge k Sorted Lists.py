d07442f56292fce7308bcaa38137d7341761bc8b
yuhuang1996 <sdu_huangyu@foxmail.com>
2019-10-10 00:10:16 +0800

    interview

--- algorithms/leetcode/LinkedList/23. Merge k Sorted Lists.py
+++ algorithms/leetcode/LinkedList/23. Merge k Sorted Lists.py
@@ -48,3 +48,42 @@ class Solution:
                 cur.next = ListNode(-1)
                 cur = cur.next
         return res
+#真正有用的方法：分治+merge 两个list
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution2:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return None
+
+        return self.merge_range_lists(lists, 0, len(lists) - 1)
+
+    def merge_range_lists(self, lists, start, end):
+        if start == end:
+            return lists[start]
+
+        mid = (start + end) // 2
+        left = self.merge_range_lists(lists, start, mid)
+        right = self.merge_range_lists(lists, mid + 1, end)
+        return self.merge_two_lists(left, right)
+
+    def merge_two_lists(self, a, b):
+        res = ListNode(0)
+        if not a:
+            return b
+        if not b:
+            return a
+
+        if a.val < b.val:
+            res.val = a.val
+            res.next = self.merge_two_lists(a.next, b)
+        else:
+            res.val = b.val
+            res.next = self.merge_two_lists(a, b.next)
+        return res
+
