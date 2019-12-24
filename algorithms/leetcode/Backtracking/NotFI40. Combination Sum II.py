2b2aafd2e32287e2792435a378b1f91e839fc3cf
yuhuang1996 <sdu_huangyu@foxmail.com>
2019-12-24 19:46:24 +0800

    interview

--- algorithms/leetcode/Backtracking/NotFI40. Combination Sum II.py
+++ algorithms/leetcode/Backtracking/NotFI40. Combination Sum II.py
@@ -1,3 +1,45 @@
+'''
+40. Combination Sum II
+Medium
+
+1240
+
+52
+
+Add to List
+
+Share
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+Accepted
+271,252
+Submissions
+606,669
+'''
 class Solution:
     def combinationSum2(self, nums, target):
         if not nums or sum(nums) < target:
